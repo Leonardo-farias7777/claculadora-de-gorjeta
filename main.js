2d9e2a1fd479c589958a74b26a6c20b1ba3bff75
leonardofribeiro1410 <leonardofribeiro1410@gmail.com>
2026-05-31 13:53:50 +0000

    setimo do html e primeiro do js

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-// Add JS here
+let conta = 0
+let pessoas = 0
+
+
+const containput = document.querySelector("#conta")
+containput.addEventListener("input", receberValorConta)
+
+function receberValorConta(evento) {
+    conta = Number(evento.target.value)
+    calcular()
+}
+
+const pessoasinput = document.querySelector("#pessoas");
+pessoasinput.addEventListener("input", receberQuantidadedepessoas)
+
+
+function receberQuantidadedepessoas(evento){
+    const paragrafoErro = document.querySelector(".pessoas #erro")
+    const divErro = document.querySelector(".pessoas .input-box")
+
+
+    if(evento.target.value === "0"){ 
+    paragrafoErro.style.display = "block"
+        divErro.setAttribute("id", "erro-div")
+    }  else{
+        paragrafoErro.style.display = "none"
+        divErro.setAttribute("id", "")
+    pessoas = Number(evento.target.value);
+    } 
+}
+ 
